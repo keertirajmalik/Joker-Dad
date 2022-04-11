7163c96c84e8abaf41bbfb605ee6c5ad6cdd5e1b
keertirajmalik <keertirajmalik@gmail.com>
2022-04-11 15:44:23 +0530

    Creating the UI for the App

--- Joker Dad/ViewController.swift
+++ Joker Dad/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var jokeView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showNewJoke(_ sender: UIButton) {
+        jokeView.text = "showing some text at the moment"
+    }
+    
 
 }
 
