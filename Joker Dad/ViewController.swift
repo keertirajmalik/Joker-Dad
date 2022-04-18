6576cdaabe14dd010a01ec2d2c40ccb18da12039
keertirajmalik <keertirajmalik@gmail.com>
2022-04-18 16:57:53 +0530

    Added animation related to card swiping when card is moved out of screen when card is moved inside screen

--- Joker Dad/ViewController.swift
+++ Joker Dad/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var jokeView: UILabel!
+    @IBOutlet var cardView: CardView!
     var jokeManager = JokeManager()
     
     override func viewDidLoad() {
@@ -18,31 +19,46 @@ class ViewController: UIViewController {
         jokeManager.delegate = self
         jokeManager.fetchJoke(with: "https://icanhazdadjoke.com")
     }
-
+    
     @IBAction func showNewJoke(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
         let point = sender.translation(in: view)
         card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
         
         if sender.state == UIGestureRecognizer.State.ended {
-            UIView.animate(withDuration: 0.2) {
+            
+            // when user move the card to the edge of the screen the card is taken out from screen
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                showNewJokeCard()
+                
+            } else if card.center.x > (view.frame.width - 75) {
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                showNewJokeCard()
+            }
+            
+            // If the card moved from centre once finger is lifted the card will snapped back to the centre again.
+            UIView.animate(withDuration: 0.4) {
                 card.center = self.view.center
             }
         }
-        
-        
     }
     
-    
-//    @IBAction func showNewJoke(_ sender: UIButton) {
-//        jokeManager.fetchJoke(with: "https://icanhazdadjoke.com")
-//    }
-
+    func showNewJokeCard() {
+            self.cardView.center = self.view.center
+            self.cardView.alpha = 1
+            self.jokeManager.fetchJoke(with: "https://icanhazdadjoke.com")
+    }
 }
 
 extension ViewController: JokeManagerDelegate {
     func fetchDadJoke(_ jokeManager: JokeManager, joke: JokeModel) {
-        
         
         DispatchQueue.main.async {
             self.jokeView.text = joke.joke
